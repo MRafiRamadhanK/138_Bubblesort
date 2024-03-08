89443c36528f3dc96998cb8719c15017108b2476
MRafiramadhanK <raficool271003@gmail.com>
2024-03-08 13:13:30 +0700

    input

--- rafiramadhanpraktik3/rafiramadhanpraktik3.cpp
+++ rafiramadhanpraktik3/rafiramadhanpraktik3.cpp
@@ -1,2 +1,27 @@
 
 #include <iostream>
+using namespace std;
+
+int a[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "Masukan banyaknya element pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "nArray dapat mempunyai maksimal 20 elemen.n";
+		}
+	}
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "masukan elemen array" << endl;
+	cout << "===================== " << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
+}
